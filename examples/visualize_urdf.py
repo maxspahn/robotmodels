1fe5fa6d990bb4dc326ce1a6267f7cee1e6b738b
Max Spahn <m.spahn@tudelft.nl>
2024-03-08 16:29:03 +0100

    fix[example]: Moves meshes to panda folder. Changes default argument in visualize.

--- examples/visualize_urdf.py
+++ examples/visualize_urdf.py
@@ -2,8 +2,8 @@ import sys
 from robotmodels.utils.robotmodel import RobotModel
 
 def main():
-    robot_name = 'kinova_gen3lite'
-    model_name = None
+    robot_name = 'kinova'
+    model_name = 'gen3lite'
     if len(sys.argv) > 1:
         robot_name = sys.argv[1]
     if len(sys.argv) > 2:
