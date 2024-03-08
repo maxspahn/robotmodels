650f4488a6051c9fe23c231d61c59f62e4a25f13
Saray <saraybakker@gmail.com>
2024-03-08 13:15:44 +0100

    make sure that visualize_urdf.py has runable arguments for robot_name and model_name

--- examples/visualize_urdf.py
+++ examples/visualize_urdf.py
@@ -2,8 +2,8 @@ import sys
 from robotmodels.utils.robotmodel import RobotModel
 
 def main():
-    robot_name = 'kinova_gen3lite'
-    model_name = None
+    robot_name = 'panda'    #or: 'kinova'
+    model_name = None       #or: 'gen3_7dof'
     if len(sys.argv) > 1:
         robot_name = sys.argv[1]
     if len(sys.argv) > 2:
