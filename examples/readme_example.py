922f8f9788f12656b1452bd7981ac5705389ccf6
Saray <saraybakker@gmail.com>
2024-03-18 10:18:03 +0100

    removed orientation that was necessary for isaac-gym but not for pybullet/etc

--- examples/readme_example.py
+++ examples/readme_example.py
@@ -1,6 +1,6 @@
 from robotmodels.utils.robotmodel import RobotModel
 
-robot_model = RobotModel(robot_name = 'panda')
+robot_model = RobotModel(robot_name = 'dingo_kinova')
 print(f"Absolute urdffile: {robot_model.get_urdf_path()}")
 print(f"Absolute xmlfile: {robot_model.get_xml_path()}")
 yourdf_model = robot_model.yourdf_model()
